12999cf4b871e5d652864ee73f99b8192ff63601
Remilekun Salami <remilekun.salami@gmail.com>
2023-02-09 18:39:21 +0100

    feat: delete note

--- screens/EditNoteScreen.tsx
+++ screens/EditNoteScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Alert, StyleSheet, View } from 'react-native';
-import { Loader, NoteForm, NoteFormValues } from '@/components';
+import { Button, Loader, NoteForm, NoteFormValues } from '@/components';
 import useNotes from '@/hooks/useNotes';
 import { Note, RootScreens, RootStackScreenProps } from '@/types';
 
@@ -9,7 +9,7 @@ const EditNoteScreen = ({
   route,
 }: RootStackScreenProps<RootScreens.EditNote>) => {
   const id = route?.params?.id;
-  const { updateNote, getNote } = useNotes();
+  const { updateNote, getNote, deleteNote } = useNotes();
   const [loading, setLoading] = useState(true);
   const [note, setNote] = useState<Note | null>(null);
 
@@ -33,6 +33,31 @@ const EditNoteScreen = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
 
+  const onDelete = () => {
+    deleteNote(id as string);
+    navigation.goBack();
+    Alert.alert('Note deleted successfully');
+  };
+
+  const onDeletePress = () => {
+    Alert.alert(
+      'Delete note',
+      'Are you sure you want to delete this note?',
+      [
+        {
+          text: "Yes, I'm sure",
+          onPress: onDelete,
+          style: 'destructive',
+        },
+        {
+          text: 'No',
+          style: 'default',
+        },
+      ],
+      { cancelable: true },
+    );
+  };
+
   const onSubmit = (values: NoteFormValues) => {
     updateNote(id as string, values);
     navigation.goBack();
@@ -44,7 +69,14 @@ const EditNoteScreen = ({
 
   return (
     <View style={styles.container}>
-      <NoteForm note={note} onSubmit={onSubmit} />
+      <NoteForm note={note} onSubmit={onSubmit}>
+        <Button
+          backgroundColor="#F8696A"
+          onPress={onDeletePress}
+          style={styles.deleteButton}>
+          Delete Note
+        </Button>
+      </NoteForm>
     </View>
   );
 };
@@ -56,5 +88,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     flex: 1,
     padding: 16,
+  },
+  deleteButton: {
+    marginTop: 16,
   },
 });
